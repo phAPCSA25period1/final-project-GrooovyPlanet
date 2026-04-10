778a563b7e85d3f65cf92c4e49e0648b87a77930
GrooovyPlanet <GrooovyPlanet@gmail.com>
2026-04-10 09:37:55 -0700

    Created classes and made event class

---
+++ src/Event.java
@@ -0,0 +1,13 @@
+public class Event {
+        /**
+         * Constuctor Variables
+         */
+        String title;
+        String date;
+
+        Event(String title, String date){
+            this.title = title;
+            this.date = date;
+        }
+
+    }
