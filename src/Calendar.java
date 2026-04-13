42129c2d827954fafc979b7d66d89988c318213d
GrooovyPlanet <GrooovyPlanet@gmail.com>
2026-04-13 09:38:32 -0700

    Completed Calendar Class with Javadoc Comments. Went back to event and added more comments and new getter methods.

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
 public class Calendar {
-    public static void main(String[] args) throws Exception {
+    private ArrayList<Event> events;
 
+    public Calendar() {
+        events = new ArrayList<>();
+    }
+
+    // Add an event
+    public void addEvent(Event event) {
+        events.add(event);
+    }
+
+    // Remove an event
+    public void removeEvent(Event event) {
+        events.remove(event);
+    }
+
+    // View all events
+    public void displayEvents() {
+        for (Event e : events) {
+            System.out.println(e);
+        }
+    }
+
+    // Optional: find events by date
+    public void getEventsByDate(String date) {
+        for (Event e : events) {
+            if (e.getDate().equals(date)) {
+                System.out.println(e);
+            }
+        }
     }
 }
--- src/Event.java
+++ src/Event.java
@@ -5,9 +5,23 @@ public class Event {
         String title;
         String date;
 
+        /**
+         * Method for creating  Event object
+         */
         Event(String title, String date){
             this.title = title;
             this.date = date;
         }
-
+        /**
+         * Method for accessing date within Event object
+         */
+        public String getDate() {
+        return date;
+        }
+        /**
+         * Method for accessing title within Event object
+         */
+        public String getTitle() {
+        return title;
     }
+}
